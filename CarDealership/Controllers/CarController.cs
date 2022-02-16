ca6c41ee92f7aff2d325a4ac8dc362c7a462279f
Brit Wallace <britwallace888@gmail.com>
2022-02-15 20:11:39 -0800

    first

---
+++ CarDealership/Controllers/CarController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+using System.Collections.Generic;
+
+namespace CarDealership.Controllers
+{
+  public class CarsController : Controller
+  {
+
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      List<Car> allItems = Car.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/cars")]
+    public ActionResult Create(string makeModel, int price, int miles)
+    {
+      Car newCar = new Car(makeModel, price, miles);
+      return RedirectToAction("Index");
+    }
+
+  }
+}
